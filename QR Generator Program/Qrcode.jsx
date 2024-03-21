c7c9fb1166b0c5e624515660713808102d4c4e9c
Nivethitha M <157672186+Nivethitha-1112@users.noreply.github.com>
2024-03-21 14:28:10 +0530

    Add files via upload

---
+++ QR Generator Program/Qrcode.jsx
@@ -0,0 +1,56 @@
+import {useState} from "react";
+
+export const Qrcode = () => {
+  const [img, setImg] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [qrData, setQrData]=useState("https://www.w3schools.com/");
+  const [qrSize, setQrSize] = useState("150");
+  
+  async function generateQR(){
+    setLoading(true);
+    try{
+      const url = `https://api.qrserver.com/v1/create-qr-code/?size=${qrSize}x${qrSize}&data=${encodeURIComponent(qrData)}`;
+      setImg(url);
+    } catch(error) {
+      console.error("Error generating QR Code", error);
+    } finally{
+          setLoading(false);
+        }
+  }
+
+  function downloadQR() {
+    fetch(img)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = "qrcode.png";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    })
+    .catch((error) => {
+      console.error("Error downloading QR Code", error);
+    });
+  }
+
+  return (
+   <div className="app-container">
+    <h1>QR Code Generator</h1>
+    {loading && <p>Please wait...</p>}
+    {img && <img src={img} className="qr-code-image"/>}
+    <div>
+        <label htmlFor="dataInput" className="input-label">
+        Data for QR Code:
+        </label>
+        <input type="text" value={qrData} id="dataInput" placeholder="Enter data for QR Code" onChange={(e) =>setQrData(e.target.value)}/>
+        <label htmlFor="sizeInput" className="input-label">
+        Image size (eg., 150):
+        </label>
+        <input type="text" value={qrSize} id="sizeInput" onChange={(e) =>setQrSize(e.target.value)}/>
+        <button className="generate-button"  disabled={loading} onClick={generateQR}>Generate QR Code</button>
+        <button className="download-button" onClick={downloadQR}>Download QR Code</button>
+    </div>
+   </div>
+  )
+}
