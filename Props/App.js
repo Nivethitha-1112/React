e922bf0d1c2bb695dae4466f7b514011aeff32a1
Nivethitha M <157672186+Nivethitha-1112@users.noreply.github.com>
2024-04-10 14:35:49 +0530

    Add files via upload

---
+++ Props/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ParentComponent from './components/ParentComponent';
+import ConditionalP from './components/ConditionalP';
+
+function App() {
+  return (
+    <div>
+      <ParentComponent />
+      <ConditionalP/>
+    </div>
+  );
+}
+
+export default App;
