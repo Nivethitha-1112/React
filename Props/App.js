c4110d5d6d9af0805de86d1e6a270537f84da5b8
Nivethitha M <157672186+Nivethitha-1112@users.noreply.github.com>
2024-04-11 11:03:36 +0530

    Add files via upload

--- Props/App.js
+++ Props/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import ParentComponent from './components/ParentComponent';
 import ConditionalP from './components/ConditionalP';
+import Welcomemessage from './components/Welcomemessage';
+import Proptypes from './components/Proptypes';
+import Passfunctions from './components/Passfunctions';
 
 function App() {
   return (
     <div>
       <ParentComponent />
       <ConditionalP/>
+      <Welcomemessage/>
+      <Proptypes/>
+      <Passfunctions/>
     </div>
   );
 }
