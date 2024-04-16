048d15bd405e55e9f7e0aff2aa6cf9f66f4c5e4b
Nivethitha M <157672186+Nivethitha-1112@users.noreply.github.com>
2024-04-16 15:24:42 +0530

    Add files via upload

--- Website Project - 01/App.js
+++ Website Project - 01/App.js
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About/About';
 import Campus from './components/Campus/Campus';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 import Programs from './components/Programs/Programs';
 import Testimonials from './components/Testimonials/Testimonials';
 import Title from './components/Title/Title';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 
 //The programs inside the container class will be applied with the css we added in index.css
-function App() {
+const App = () => {
+
+  const [playState, setPlayState] = useState(false);
+
+
   return (
     <div>
       <Navbar />
@@ -16,12 +24,16 @@ function App() {
       <div className='container'>
         <Title subTitle ='OUR PROGRAM' title='What We Offer'/>
         <Programs />
-        <About/>
+        <About setPlayState={setPlayState} />
         <Title subTitle ='Gallery' title='Campus Photos'/>
         <Campus/>
         <Title subTitle ='TESTIMONIALS' title='What Student Says'/>
         <Testimonials/>
+        <Title subTitle ='Contact Us' title='Get in Touch'/>
+        <Contact/>
+        <Footer/>
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   );
 }
