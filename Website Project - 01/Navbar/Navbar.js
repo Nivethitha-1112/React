b1fe037c0c8852256ac0b68967ce304838542195
Nivethitha M <157672186+Nivethitha-1112@users.noreply.github.com>
2024-04-16 15:24:14 +0530

    Add files via upload

--- Website Project - 01/Navbar/Navbar.js
+++ Website Project - 01/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-scroll'; //import link from react scroll (website refered: react-scroll-npm)
 import './Navbar.css'
 import logo from '../../assets/logo.png' //importing the logo from assets folder
+import menu_icon from '../../assets/menu-icon.png'
 
 const Navbar = () => {
 
@@ -16,17 +18,25 @@ const Navbar = () => {
     //first argument to useEffect is a function contains the code which will run after each render
     //second argument is array dependencies which is empty array, which will run once after initial render
     //we add an event listener to the window's scroll event. Whenever the user scrolls, this event listener checks the vertical scroll position (window.scrollY). If it's greater than 50 pixels, it sets the sticky state to true, indicating that the Navbar should become sticky. Otherwise, it sets the sticky state to false.
+    
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
+
+
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="" className='logo' />
-            <ul>
-                <li>Home</li>
-                <li>Program</li>
-                <li>About us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
-                <li><button className='btn'>Contact Us</button></li>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+                <li><Link to='hero' smooth={true} offset={0} duration={500}> Home</Link></li>
+                <li><Link to='program' smooth={true} offset ={-260} duration={500}>Programs</Link></li>
+                <li><Link to='about' smooth={true} offset ={-150} duration={500}>About us</Link></li>
+                <li><Link to='campus' smooth={true} offset ={-260} duration={500}>Campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset ={-260} duration={500}>Testimonials</Link></li>
+                <li><Link to='contact' smooth={true} offset ={-260} duration={500} className='btn'>Contact Us</Link></li>
             </ul>
+            <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
         </nav>
     )
 }
