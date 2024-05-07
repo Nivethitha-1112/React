a15f9daa2c462558763959844a0e6411f1576bcd
Nivethitha M <157672186+Nivethitha-1112@users.noreply.github.com>
2024-05-07 14:47:23 +0530

    Add files via upload

---
+++ Descriptionbox/Descriptionbox.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Descriptionbox.css'
+
+const Descriptionbox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className='descriptionbox-navigator'>
+                <div className='descriptionbox-nav-box'>Description</div>
+                <div className='descriptionbox-nav-box fade'>Reviews (122)</div>
+            </div>
+            <div className='descriptionbox-description'>
+                <p> Welcome to SHOPPER e-commerce website, your gateway to
+                    convenience and choice in the digital marketplace. We're here to
+                    redefine the way you shop, offering a seamless online platform where you can
+                    explore, discover, and purchase a wide array of products
+                    and services from the comfort of your own home. Whether
+                    you're seeking the latest trends, essential household items,
+                    or unique gifts, we provide a convenient and secure
+                    shopping experience tailored to your needs. With easy
+                    navigation, secure payment options, and reliable delivery
+                    services, we're committed to making your online shopping
+                    experience enjoyable and hassle-free. Join us in embracing
+                    the future of retail, where possibilities are limitless
+                    and convenience is paramount.</p>
+                <p>
+                    Your ultimate destination for chic dresses in the world
+                    of e-commerce. Browse our curated collection of stunning
+                    dresses for every occasion, from elegant evening gowns
+                    to casual day dresses. With our easy-to-use website,
+                    secure checkout, and fast shipping, finding your perfect
+                    dress has never been easier. Experience the thrill of
+                    shopping for fashion-forward styles from the comfort
+                    of your own home.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Descriptionbox
